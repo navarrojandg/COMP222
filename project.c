aa4e30d12562e2a42aeebe50f2cb046d69b99695
navarrojandg <navarrojanderrick@gmail.com>
2021-05-08 11:09:44 -0700

    feat/ finished IEEE 754 conversion project

--- project.c
+++ project.c
@@ -1,19 +1,53 @@
 #include <stdio.h>
 #include <math.h>
 
+// helper function to help visualize IEEE Bits
+void printBits(unsigned x) {
+    for (signed i = 31; i > -1; i--) {
+        if (i == 30 || i == 22) { printf(" "); };
+        (x & (1 << i)) ? printf("1") : printf("0");
+    };
+    printf("\n");
+};
+
 //----------------------------
 // Part 1:  IEEE to Float
 //----------------------------
 double getFraction(unsigned x) {
-    return 0.0;
+    // 0 00000000 00000000000000000000000
+    // 0 00000000 11111111111111111111111
+    // 0x007FFFFF
+    // mask to extract bits 0-22
+
+    unsigned mantissa = x & 0x7FFFFF; // isolate the mantissa bits
+    double acc = 1.0;
+    double j = 0.5;
+    for (signed i = 22; i > -1; i--, j /= 2) {
+        if ((mantissa & (1 << i))) { acc += j; }; // if bit is 1 then we add j to the accumulator;
+    };
+
+    return acc;
 };
 
 int getExponent(unsigned x) {
-    return 0;
+    // 0 00000000 00000000000000000000000
+    // 0 11111111 00000000000000000000000
+    // 0x7F800000
+    // mask to extract bits 23-30
+    
+    unsigned exponent = x & 0x7F800000; // isolate the exponent bits;
+    int acc = 0;
+    int j = 1;
+    for (signed i = 23; i < 31; i++, j *= 2) {
+        if ((exponent & (1 << i))) { acc += j; }; // if bit is 1 then we add j to the accumulator;
+    };
+
+    return acc - 127;
 };
 
 double getFloat(unsigned x) {
-    return 0.0;
+    double acc = getFraction(x) * (pow(2, getExponent(x))); 
+    return (x & (1 << 31)) ? (acc * -1) : acc; // handle the sign before returning acc
 };
 
 //----------------------------------
@@ -29,15 +63,29 @@ double getNormfrac(double x) {
 };
 
 unsigned getBinfrac(double x) {
-    return 0x0;
+    double normFrac = getNormfrac(x) - 1; // get normalized fraction and remove the leading 1;
+    unsigned acc = 0x0;
+    for (int i = 0; i < 22; i++, acc = acc << 1) {// add 1 to the acc if needed and shift the bits each increment;
+        normFrac *= 2;
+        if (floor(normFrac) == 1) {
+            acc += 0x1;
+            normFrac -= 1;
+        };
+    };
+    return acc;
 };
 
 unsigned getBinexp(double x) {
-    return 0x0;
+    unsigned normExp = getNormexp(x) + 127;
+    return normExp << 23;
 };
 
 unsigned getIEEE(double x) {
-    return 0x0;
+    unsigned acc = 0x0;
+    if (x < 0) { acc += (1 << 31); }; // sign bit;
+    acc += getBinexp(x);
+    acc += getBinfrac(x);
+    return acc;
 };
 
 //-------------------------------
